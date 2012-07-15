b0c21d43fa7236157896125aec0adcbb6ecf346c
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-15 16:34:16 -0700

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715225806) do
+ActiveRecord::Schema.define(:version => 20120715230610) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name",       :null => false
+    t.integer  "user_id",    :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "projects", ["user_id"], :name => "index_projects_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
