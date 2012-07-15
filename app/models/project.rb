724b64a151ff27dfe367753553f0ea084c60512c
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-15 16:33:19 -0700

    Project db and model validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :user_id
 
   belongs_to :user
+
+  # Not blank and unique to user
+  validates :name, :presence => true, :uniqueness => {:scope => :user_id}
 end
--- db/migrate/20120715230610_create_projects.rb
+++ db/migrate/20120715230610_create_projects.rb
@@ -1,10 +1,12 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-      t.string :name
-      t.integer :user_id
+      t.string :name, :null => false
+      t.integer :user_id, :null => false
 
       t.timestamps
     end
+
+    add_index :projects, :user_id
   end
 end
