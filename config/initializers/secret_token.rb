8c47b93f3fb787194fc8815291aedc3591ad2111
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-15 14:59:05 -0700

    rails new appian

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appian::Application.config.secret_token = '333f2d51905830af17ce92f3724e6acd3cac405a5c5845f17e1ff99379f6d4d2600e1a521893e08cf7f564bdf73bc2d0c8d3fc41c78c9f3f41a7f15a1cf61191'
